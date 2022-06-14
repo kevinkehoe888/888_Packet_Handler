8a182f6e1af6c0c3feac9a9f94ad0543b80188a3
Kevin Kehoe <72750511+kevinkehoe888@users.noreply.github.com>
2022-06-14 12:50:48 +0100

    Adding events working.
    Need to add some checks on the dates / based on supplier limit calender to specific dates - or no dates at all for some suppliers.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import paramiko
 # How to build
 # pyinstaller --onefile --hidden-import babel.numbers --windowed 888_Packet_Handler.py
 
-known_host_servers, known_host_optionMenu = [], []
+known_host_servers, known_host_optionMenu, events = ["jump.spectateprod.com", "jump.spectateprodusa.com", "jump.spectatemirage.com"], ["TEST1", "TEST2", "TEST3"], []
 date_labels, dates = {}, {}
 successful_login, date_counter, date_labels_y_pos = 0, 0, 0
 
@@ -32,6 +32,7 @@ supplier_options = [
   ["BGIN_SC", "BetGenius_SC"]
 ]
 
+# This function will test logins on all servers. If its successful the server can be chosen later.
 def login_to_server(username, password, successful_login):
     username_input.delete(0, "end")
     username_input["state"] = "disabled"
@@ -93,13 +94,13 @@ def login_to_server(username, password, successful_login):
         chosen_options_value = StringVar(root)
         chosen_options_value.set(supplier_options[2][1])
         options = OptionMenu(root, chosen_options_value, supplier_options[0][1], supplier_options[1][1], supplier_options[2][1], supplier_options[3][1], supplier_options[4][1], supplier_options[5][1], supplier_options[6][1], supplier_options[7][1], supplier_options[8][1], supplier_options[9][1], supplier_options[10][1], supplier_options[11][1], supplier_options[12][1], supplier_options[13][1], supplier_options[14][1])#, command=date_disabler)
-        options.place(x=115, y=270)
+        options.place(x=485, y=13)
 
         # Loading Server Dropdown
         chosen_server_value = StringVar(root)
         chosen_server_value.set(known_host_optionMenu[0])
         server_options = OptionMenu(root, chosen_server_value, *known_host_optionMenu)
-        server_options.place(x=350, y=270)
+        server_options.place(x=720, y=13)
         print(f"SUCCESSFUL LOGINS {succesful_login}")
 
 # This function will allow The Labels and DateEntrys to be added with their own keys. Ex - date_label1 or date_12 etc.
@@ -138,6 +139,53 @@ def delete_date_function():
   if delete_date_button["state"] == "normal" and date_counter == 0:
     delete_date_button["state"] = "disabled"
 
+def add_event_details_function(supplier, server, event_id, feed_event_id, dates):
+    temp_dates = []
+    # Set supplier index
+    for index, value in enumerate(supplier_options):
+        if supplier == value[1]:
+            print(f"{supplier} is at index {index}")
+            supplier = index
+            break
+    
+    # Set Server address
+    for index, value in enumerate(known_host_optionMenu):
+        if server == value:
+            print(f"{server} is at index {index}")
+            server = known_host_servers[index]
+            break
+
+    if not event_id:
+        print("Please include an Event ID")
+        return
+
+    if not feed_event_id:
+        print("Please include an Feed Event ID")
+        return
+
+    for index, value in dates.items():
+        temp_dates.append(str(value.get_date()))
+
+    print(supplier, server, event_id, feed_event_id, temp_dates)
+    events.append([supplier, server, event_id, feed_event_id, temp_dates])
+    print(events)
+
+
+# TEST LABELS
+#Loading Supplier Dropdown
+chosen_options_value = StringVar(root)
+chosen_options_value.set(supplier_options[2][1])
+options = OptionMenu(root, chosen_options_value, supplier_options[0][1], supplier_options[1][1], supplier_options[2][1], supplier_options[3][1], supplier_options[4][1], supplier_options[5][1], supplier_options[6][1], supplier_options[7][1], supplier_options[8][1], supplier_options[9][1], supplier_options[10][1], supplier_options[11][1], supplier_options[12][1], supplier_options[13][1], supplier_options[14][1])#, command=date_disabler)
+options.place(x=485, y=13)
+
+# Loading Server Dropdown
+chosen_server_value = StringVar(root)
+chosen_server_value.set(known_host_optionMenu[0])
+server_options = OptionMenu(root, chosen_server_value, *known_host_optionMenu)
+server_options.place(x=720, y=13)
+
+# END OF TEST LABELS
+
 username_label = Label(root, text="Username")
 username_label.place(x=30,y=30)
 username_input = Entry(root, width=30)
@@ -160,25 +208,25 @@ server_label.place(x=670,y=18)
 event_id_label = Label(root, text="Event ID")
 event_id_label.place(x=400, y=50)
 event_id_input = Entry(root, width=50)
-event_id_input["state"] = "disabled"
+#event_id_input["state"] = "disabled"
 event_id_input.place(x=490, y=50)
 
 feed_event_id_label = Label(root, text="Feed Event ID")
 feed_event_id_label.place(x=400, y=80)
 feed_event_id_input = Entry(root, width=50)
-feed_event_id_input["state"] = "disabled"
+#feed_event_id_input["state"] = "disabled"
 feed_event_id_input.place(x=490,y=80)
 
 add_date_button = Button(root, text="Add Date Field", command=add_date_function)
-add_date_button["state"] = "disabled"
+#add_date_button["state"] = "disabled"
 add_date_button.place(x=400,y=110)
 
 delete_date_button = Button(root, text="Delete Date Field", command=delete_date_function)
-delete_date_button["state"] = "disabled"
+#delete_date_button["state"] = "disabled"
 delete_date_button.place(x=510,y=110)
 
-add_event_details = Button(root, text="Add Event")#, command=add_event_details_function)
-add_event_details["state"] = "disabled"
+add_event_details = Button(root, text="Add Event", command=lambda:add_event_details_function(chosen_options_value.get(), chosen_server_value.get(), event_id_input.get(), feed_event_id_input.get(), dates))
+#add_event_details["state"] = "disabled"
 add_event_details.place(x=630,y=110)
 
 start_gathering_packets_details = Button(root, text="Start Packet Gathering")#, command=lambda:threading.Thread(target=start_gathering_packets_details_functions).start())
